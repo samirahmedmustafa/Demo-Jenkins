a23075ea54a23c22fc28e6df0b4503354a16eeeb
Samir Mustafa <samir.ahmedmustafa@gmail.com>
2024-02-13 14:51:37 -0500

    first commit

---
+++ src/main/java/com/example/entity/Controller.java
@@ -0,0 +1,15 @@
+package com.example.entity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/items")
+public class Controller {
+
+	@GetMapping
+	public String getItems() {
+		return "Hello API!";
+	}
+}
